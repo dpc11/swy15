429bf2ebde28a58ef802b28ca6e2a7ee948c44c6
Dai <dpc11@qq.com>
2025-01-15 01:25:46 +0900

    update: desc

--- images/image.py
+++ images/image.py
@@ -1,9 +1,12 @@
 import os
 
 images = ""
-for subdir, dirs, files in os.walk("C:\\Users\\dpc11\\Documents\\swy15\\images\\school\\素描"):
+for subdir, dirs, files in os.walk("C:\\Users\\dpc11\\Documents\\swy15\\images\\freelance\\商業用原稿"):
     for file in files:
         print( """{{
-            src: '../images/school/素描/{}',
-            thumb: '../images/school/素描/{}',
-            }},""".format(file, file))
+            src: '../images/freelance/商業用原稿/{}',
+            thumb: '../images/freelance/商業用原稿/{}',
+             subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">{}</h3>
+            </div>`,
+            }},""".format(file, file, file))
--- js/freelance-gallery.js
+++ js/freelance-gallery.js
@@ -20,65 +20,103 @@ const inlineGallery = lightGallery(lgContainer, {
     plugins: [lgZoom, lgThumbnail, lgAutoplay],
     dynamicEl: [
         {
-            src: '../images/freelance/商業用原稿/夜の街で、3人の少女がバイクに乗って集まっています。彼女たちはストリートアートの前で秘密の会話をしています。.png',
-            thumb: '../images/freelance/商業用原稿/夜の街で、3人の少女がバイクに乗って集まっています。彼女たちはストリートアートの前で秘密の会話をしています。.png',
-        },
-        {
             src: '../images/freelance/商業用原稿/2人の少女が水中で横たわり、後ろには中秋の月と遠くの都市が見えます。.png',
             thumb: '../images/freelance/商業用原稿/2人の少女が水中で横たわり、後ろには中秋の月と遠くの都市が見えます。.png',
             subHtml: `<div class="lightGallery-captions">
-                <h3>2人の少女が水中で横たわり、後ろには中秋の月と遠くの都市が見えます。</h3>
+                <h3 style="color: #ffffff">2人の少女が水中で横たわり、後ろには中秋の月と遠くの都市が見えます。.png</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/2人の美少女が空中を飛んでいる姿を描きました。.jpg',
+            thumb: '../images/freelance/商業用原稿/2人の美少女が空中を飛んでいる姿を描きました。.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">2人の美少女が空中を飛んでいる姿を描きました。.jpg</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/『光と夜の恋』の萧逸が高級アパートのソファで思索しているシーンを描きました。窓の外には夜空の下に広がる都市が広がっています。.jpg',
+            thumb: '../images/freelance/商業用原稿/『光と夜の恋』の萧逸が高級アパートのソファで思索しているシーンを描きました。窓の外には夜空の下に広がる都市が広がっています。.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">『光と夜の恋』の萧逸が高級アパートのソファで思索しているシーンを描きました。窓の外には夜空の下に広がる都市が広がっています。.jpg</h3>        
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/あるスマートフォンのマスコット美少女のイラストで、私は彼女のクリスマス特集のスマホ壁紙を担当しました。.jpg',
+            thumb: '../images/freelance/商業用原稿/あるスマートフォンのマスコット美少女のイラストで、私は彼女のクリスマス特集のスマホ壁紙を担当しました。.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">あるスマートフォンのマスコット美少女のイラストで、私は彼女のクリスマス特集のスマホ壁紙を担当しました。.jpg</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/こちらのイラストは、『三国殺-幻想大陸』というゲームに登場する曹操を描いたものです。KVアニメーションのシーンの中で、彼が血を浴びながら戦う姿を表現しました。.jpg',
+            thumb: '../images/freelance/商業用原稿/こちらのイラストは、『三国殺-幻想大陸』というゲームに登場する曹操を描いたものです。KVアニメーションのシーンの中で、彼が血を浴びながら戦う姿を表現しました。.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">こちらのイラストは、『三国殺-幻想大陸』というゲームに登場する曹操を描いたものです。KVアニメーションのシーンの中で、彼が血を浴びながら戦う姿を表現しました。.jpg</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/こちらのイラストは、アニメ『斬赤紅之瞳』のエスデスを描いた同人イラストです。彼女が満月の下で戦う姿を描きました。.jpg',
+            thumb: '../images/freelance/商業用原稿/こちらのイラストは、アニメ『斬赤紅之瞳』のエスデスを描いた同人イラストです。彼女が満月の下で戦う姿を描きました。.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">こちらのイラストは、アニメ『斬赤紅之瞳』のエスデスを描いた同人イラストです。彼女が満月の下で戦う姿を描きました。.jpg</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/こちらはその美少女の別のPC壁紙です。.jpg',
+            thumb: '../images/freelance/商業用原稿/こちらはその美少女の別のPC壁紙です。.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">こちらはその美少女の別のPC壁紙です。.jpg</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/このメカモンスターの威圧的な形態を描きました。背景は3Dで描いています。.png',
+            thumb: '../images/freelance/商業用原稿/このメカモンスターの威圧的な形態を描きました。背景は3Dで描いています。.png',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">このメカモンスターの威圧的な形態を描きました。背景は3Dで描いています。.png</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/これは『夢の新大陸』というモバイルゲームの中国新年特別ホームページイラストです。.jpg',
+            thumb: '../images/freelance/商業用原稿/これは『夢の新大陸』というモバイルゲームの中国新年特別ホームページイラストです。.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">これは『夢の新大陸』というモバイルゲームの中国新年特別ホームページイラストです。.jpg</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/夜の街で、3人の少女がバイクに乗って集まっています。彼女たちはストリートアートの前で秘密の会話をしています。.png',
+            thumb: '../images/freelance/商業用原稿/夜の街で、3人の少女がバイクに乗って集まっています。彼女たちはストリートアートの前で秘密の会話をしています。.png',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">夜の街で、3人の少女がバイクに乗って集まっています。彼女たちはストリートアートの前で秘密の会話をしています。.png</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/小さなロリィの夏の水着姿で、浮き輪に座っている立ち絵です。このイラストでは、浮き輪と水の質感を表現したいと思いました。.png',
+            thumb: '../images/freelance/商業用原稿/小さなロリィの夏の水着姿で、浮き輪に座っている立ち絵です。このイラストでは、浮き輪と水の質感を表現したいと思いました。.png',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">小さなロリィの夏の水着姿で、浮き輪に座っている立ち絵です。このイラストでは、浮き輪と水の質感を表現したいと思いました。.png</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/少女の立ち絵1.jpg',
+            thumb: '../images/freelance/商業用原稿/少女の立ち絵1.jpg',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">少女の立ち絵1.jpg</h3>
+            </div>`,
+        },
+        {
+            src: '../images/freelance/商業用原稿/少女の立ち絵2.png',
+            thumb: '../images/freelance/商業用原稿/少女の立ち絵2.png',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">少女の立ち絵2.png</h3>
             </div>`,
         },
         {
             src: '../images/freelance/商業用原稿/鏡の中の少女がカメラを見つめるレトロで華やかなイラストを描きました。背景は3ds Maxを使用して制作しました。.png',
             thumb: '../images/freelance/商業用原稿/鏡の中の少女がカメラを見つめるレトロで華やかなイラストを描きました。背景は3ds Maxを使用して制作しました。.png',
+            subHtml: `<div class="lightGallery-captions">
+                <h3 style="color: #ffffff">鏡の中の少女がカメラを見つめるレトロで華やかなイラストを描きました。背景は3ds Maxを使用して制作しました。.png</h3>
+            </div>`,
         },
-        {
-            src: '../images/freelance/商業用原稿/これは『夢の新大陸』というモバイルゲームの中国新年特別ホームページイラストです。.jpg',
-            thumb: '../images/freelance/商業用原稿/これは『夢の新大陸』というモバイルゲームの中国新年特別ホームページイラストです。.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/2人の美少女が空中を飛んでいる姿を描きました。.jpg',
-            thumb: '../images/freelance/商業用原稿/2人の美少女が空中を飛んでいる姿を描きました。.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/『光と夜の恋』の萧逸が高級アパートのソファで思索しているシーンを描きました。窓の外には夜空の下に広がる都市が広がっています。.jpg',
-            thumb: '../images/freelance/商業用原稿/『光と夜の恋』の萧逸が高級アパートのソファで思索しているシーンを描きました。窓の外には夜空の下に広がる都市が広がっています。.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/あるスマートフォンのマスコット美少女のイラストで、私は彼女のクリスマス特集のスマホ壁紙を担当しました。.jpg',
-            thumb: '../images/freelance/商業用原稿/あるスマートフォンのマスコット美少女のイラストで、私は彼女のクリスマス特集のスマホ壁紙を担当しました。.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/こちらのイラストは、『三国殺-幻想大陸』というゲームに登場する曹操を描いたものです。KVアニメーションのシーンの中で、彼が血を浴びながら戦う姿を表現しました。.jpg',
-            thumb: '../images/freelance/商業用原稿/こちらのイラストは、『三国殺-幻想大陸』というゲームに登場する曹操を描いたものです。KVアニメーションのシーンの中で、彼が血を浴びながら戦う姿を表現しました。.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/こちらのイラストは、アニメ『斬赤紅之瞳』のエスデスを描いた同人イラストです。彼女が満月の下で戦う姿を描きました。.jpg',
-            thumb: '../images/freelance/商業用原稿/こちらのイラストは、アニメ『斬赤紅之瞳』のエスデスを描いた同人イラストです。彼女が満月の下で戦う姿を描きました。.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/こちらはその美少女の別のPC壁紙です。.jpg',
-            thumb: '../images/freelance/商業用原稿/こちらはその美少女の別のPC壁紙です。.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/このメカモンスターの威圧的な形態を描きました。背景は3Dで描いています。.png',
-            thumb: '../images/freelance/商業用原稿/このメカモンスターの威圧的な形態を描きました。背景は3Dで描いています。.png',
-        },
-        {
-            src: '../images/freelance/商業用原稿/小さなロリィの夏の水着姿で、浮き輪に座っている立ち絵です。このイラストでは、浮き輪と水の質感を表現したいと思いました。.png',
-            thumb: '../images/freelance/商業用原稿/小さなロリィの夏の水着姿で、浮き輪に座っている立ち絵です。このイラストでは、浮き輪と水の質感を表現したいと思いました。.png',
-        },
-        {
-            src: '../images/freelance/商業用原稿/少女の立ち絵1.jpg',
-            thumb: '../images/freelance/商業用原稿/少女の立ち絵1.jpg',
-        },
-        {
-            src: '../images/freelance/商業用原稿/少女の立ち絵2.png',
-            thumb: '../images/freelance/商業用原稿/少女の立ち絵2.png',
-        },
-
     ],
     thumbWidth: 100,
     thumbHeight: "80px",
